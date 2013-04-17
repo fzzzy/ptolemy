11e30405df24c4221c154dce667cacfa7021d338
Julian Viereck <julian.viereck@gmail.com>
2013-04-17 17:40:46 +0200

    Remove not yet existing src/ directory

--- make.js
+++ make.js
@@ -47,8 +47,7 @@ target.lint = function() {
   echo('### Linting JS files');
 
   var LINT_FILES = ['make.js',
-                    'external/crlfchecker/',
-                    'src/'
+                    'external/crlfchecker/'
                     ];
 
   var jshintPath = path.normalize('./node_modules/.bin/jshint');
