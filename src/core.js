bca57e70952a4c76dd4cb54a553076ea7b7b308c
Julian Viereck <julian.viereck@gmail.com>
2013-04-17 20:36:00 +0200

    Add support to disable worker and run fake worker from main thread for easier debugging. Fix indention in core.js file as well

--- src/core.js
+++ src/core.js
@@ -16,24 +16,38 @@ if (!globalScope.PTOLEMY) {
   globalScope.PTOLEMY = {};
 }
 
-function setupMainThreadMessageHandler() {
-    var worker = new Worker(globalScope.PTOLEMY.workerSrc);
-    var handler = new MessageHandler('main', worker);
+globalScope.PTOLEMY.enableWorker = true;
 
-    return handler;
+function setupMainThreadMessageHandler() {
+  var worker;
+  if (globalScope.PTOLEMY.enableWorker) {
+    worker = new Worker(globalScope.PTOLEMY.workerSrc);
+  } else {
+    worker = {
+      postMessage: function WorkerTransport_postMessage(obj) {
+        worker.onmessage({data: obj});
+      },
+      terminate: function WorkerTransport_terminate() {}
+    };
+  }
+  var handler = new MessageHandler('main', worker);
+
+  if (!globalScope.PTOLEMY.enableWorker) {
+    setupWorkerMessageHandler(handler);
+  }
+
+  return handler;
 }
 
-function setupWorkerMessageHandler() {
-    var handler = new MessageHandler('worker', globalScope);
-
-    handler.on('ping', function(data, promise) {
-        console.log('Hello World from WebWorker');
-        console.warn('Hello World from WebWorker');
-        setTimeout(function() {
-            promise.resolve(data + ' World');
-            console.error('Hello World from WebWorker');
-        }, 250);
-    });
+function setupWorkerMessageHandler(handler) {
+  handler.on('ping', function(data, promise) {
+    console.log('Hello World from WebWorker');
+    console.warn('Hello World from WebWorker');
+    setTimeout(function() {
+      promise.resolve(data + ' World');
+      console.error('Hello World from WebWorker');
+    }, 250);
+  });
 }
 
 globalScope.PTOLEMY.boot = function boot() {
@@ -41,7 +55,8 @@ globalScope.PTOLEMY.boot = function boot() {
   if (isWorker) {
     globalScope.console = workerConsole;
 
-    setupWorkerMessageHandler();
+    var handler = new MessageHandler('worker', globalScope);
+    setupWorkerMessageHandler(handler);
   } else {
     globalScope.PTOLEMY.handler = setupMainThreadMessageHandler();
   }
