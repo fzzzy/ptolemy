2d6fa0a9711af60200173f284faec46eaf517684
Julian Viereck <julian.viereck@gmail.com>
2013-12-19 16:16:49 +0100

    Removing Leaflet and IDBWrapper source from the repo - only keep the "compiled" sources under external/

--- local-server.js
+++ local-server.js
@@ -1,8 +1,10 @@
+var port = 8888;
+
 var connect = require('connect');
 connect.createServer(
     connect.static(__dirname)
-).listen(8888);
+).listen(port);
 
 console.log('### Running local server');
 console.log('');
-console.log('Running server at localhost:8888');
+console.log('Running server at localhost:' + port);
