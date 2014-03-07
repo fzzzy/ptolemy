1802d66d05c2a152e60d28a10140150d0bdd3b84
Nick Desaulniers <ndesaulniers@mozilla.com>
2014-03-06 17:05:50 -0800

    request mapforge data

--- local-server.js
+++ local-server.js
@@ -4,23 +4,44 @@
 
 var port = 8888;
 
+var fs = require('fs');
+var spawn = require('child_process').spawn;
 var connect = require('connect');
 var fetchOsmData = require('./fetch_osm_data');
+
+function errRes (res, msg) {
+  res.statusCode = 404;
+  res.end();
+};
+
 var app = connect()
           .use(connect.static(__dirname))
           .use(connect.query())
           .use(function (req, res) {
+  console.log('got a request: ' + req.url);
   var query = req.query;
   var s = query.s;
   var w = query.w;
   var n = query.n;
   var e = query.e;
-  if ([s, w, n, e].every(isFinite)) {
-    fetchOsmData(s, w, n, e).pipe(res);
-  } else {
-    res.statusCode = 404;
-    res.end("expected numeric querystring parameters s, w, n, & e");
+  if (![s, w, n, e].every(isFinite)) {
+    return errRes("expected numeric querystring parameters s, w, n, & e");
   }
+  var osmFile = fs.createWriteStream('temp.osm');
+  osmFile.on('finish', function () {
+    var osmosis = spawn('osmosis-0.40.1/bin/osmosis', [
+      '--read-xml', 'file=temp.osm', '--mapfile-writer', 'file=mymap.map',
+      'bbox=' + s + ',' + w + ',' + n + ',' + e
+    ]);
+
+    osmosis.stdout.on('data', function (d) { console.log(d); });
+    osmosis.stderr.on('data', function (e) { console.error(e.toString()); });
+    osmosis.on('close', function (exitCode) {
+      if (exitCode !== 0) return errRes("osmosis exited with code: " + exitCode);
+      fs.createReadStream('mymap.map').pipe(res);
+    });
+  });
+  fetchOsmData(s, w, n, e).pipe(osmFile);
 });
 
 require('http').createServer(app).listen(port);
